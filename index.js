46f5fa19562aad2769135d591cb4e418bb9233c3
Sergey Stupachenko <bumburumburum@gmail.com>
2017-05-04 21:56:00 +0300

    Added JSX files to process

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var hook = require('node-hook');
-
-hook.hook('.js', (src, name) => {
+const hookFn = (src, name) => {
   src = src.replace(/import ([^{]*?) from '(.*?)'/g, 'const $1 = require("$2")');
   src = src.replace(/export default ([^ ]*)/g, 'module.exports = $1');
   src = src.replace(/export (var|let|const) ([a-zA-Z0-9_$]*)/g, '$1 $2 = module.exports.$2');
@@ -12,4 +11,7 @@ hook.hook('.js', (src, name) => {
       .join('');
   });
   return src;
-});
+};
+
+hook.hook('.js', hookFn);
+hook.hook('.jsx', hookFn);
