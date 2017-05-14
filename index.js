4791e5fa47124ca1698a6bf8c476345878ce7aac
Jim Driscoll <jim.a.driscoll@gmail.com>
2017-05-14 17:06:13 +0100

    Support exporting a series of names

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ hook.hook('.js', (src, name) => {
   src = src.replace(/export (var|let|const) ([a-zA-Z0-9_$]*)/g, '$1 $2 = module.exports.$2');
   src = src.replace(/export function ([a-zA-Z0-9_$]*)/g, 'var $1 = module.exports.$1 = function');
   src = src.replace(/export class ([a-zA-Z0-9_$]*)/g, 'var $1 = module.exports.$1 = class');
+  src = src.replace(/export {(.*?)}/g, (all, $1) => {
+    return $1.split(/,/).map(name => `module.exports.${name.trim()} = ${name.trim()}`).join(";");
+  });
   src = src.replace(/import {(.*?)} from (["'])(.*?)\2/g, (all, $1, $2, $3) => {
     return $1.split(",")
       .map(part => 'var ' + part + '= require("' + $3 + '").' + part.trim() + ';')
