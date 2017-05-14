38ecce05c437ea75785e0f7bc40d97d62882b3d4
Jim Driscoll <jim.a.driscoll@gmail.com>
2017-05-14 17:04:11 +0100

    Hoist: use a single "imported" namespace rather than runtime assignment

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var hook = require('node-hook');
 
 hook.hook('.js', (src, name) => {
-  src = src.replace(/import ([^{]*?) from (["'])(.*?)\2/g, 'const $1 = require("$3")');
+  var seen_names = {};
+  src = src.replace(/import ([^{]*?) from (["'])(.*?)\2/g, (all, $1, $2, $3) => {
+    seen_names[$1.trim()] = $3;
+    return `imported["${$3}"] = require("${$3}")`;
+  });
   src = src.replace(/export default ([^ ]*)/g, 'module.exports = $1');
   src = src.replace(/export (var|let|const) ([a-zA-Z0-9_$]*)/g, '$1 $2 = module.exports.$2');
   src = src.replace(/export function ([a-zA-Z0-9_$]*)/g, 'var $1 = module.exports.$1 = function');
@@ -10,9 +14,14 @@ hook.hook('.js', (src, name) => {
     return $1.split(/,/).map(name => `module.exports.${name.trim()} = ${name.trim()}`).join(";");
   });
   src = src.replace(/import {(.*?)} from (["'])(.*?)\2/g, (all, $1, $2, $3) => {
-    return $1.split(",")
-      .map(part => 'var ' + part + '= require("' + $3 + '").' + part.trim() + ';')
-      .join('');
+    $1.split(",").forEach(name => {
+        seen_names[name.trim()] = $3;
+    });
+    return `imported["${$3}"] = require("${$3}")`;
   });
-  return src;
+  // Ultimately we want AST parsing but this will work in most cases.
+  Object.keys(seen_names).forEach(name => {
+    src = src.replace(new RegExp(`\\b${name}\\b`, "g"), `imported["${seen_names[name]}"].${name}`)
+  });
+  return "var imported = {};" + src;
 });
