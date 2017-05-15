291a88ac453bde70670096d0cf276ed23cea9a4d
Jim Driscoll <jim.a.driscoll@gmail.com>
2017-05-15 22:38:19 +0100

    Instead of using a variable to carry hoisted declarations, use two passes (define and assign)

--- index.js
+++ index.js
@@ -1,34 +1,94 @@
 var hook = require('node-hook');
 
 hook.hook('.js', (src, name) => {
-  var seen_names = {};
-  src = src.replace(/\bimport ([^{]*?) from (["'])(.*?)\2/g, (all, $1, $2, $3) => {
-    seen_names[$1.trim()] = $3;
-    return `imported["${$3}"] = require("${$3}").ns`;
-  });
-  src = src.replace(/\bimport {(.*?)} from (["'])(.*?)\2/g, (all, $1, $2, $3) => {
-    $1.split(",").forEach(name => {
-        seen_names[name.trim()] = $3;
-    });
-    return `imported["${$3}"] = require("${$3}").ns`;
-  });
-  if(Object.keys(seen_names).length) {
-      // Ultimately we want AST parsing but this will work in most cases.
-      Object.keys(seen_names).forEach(name => {
-        src = src.replace(new RegExp(`\\b${name}\\b`, "g"), `imported["${seen_names[name]}"].${name}`)
-      });
-      src = "var imported = {};" + src;
+  /* How this works:
+   *
+   * At a simple level, `import {Foo} from "./foo"` is the same as
+   * `var Foo = require("./foo").Foo`. Unfortunately it's not quite that simple because
+   * "export" hoists (its names exist before the code delaring them is executed), which is
+   * very valuable when you have circular references but needs to be emulated here.
+   *
+   * Instead, the code `import {Foo} from "./foo"` becomes roughly:
+   *
+   *     var Foo = require("./foo").Foo; // Original line position
+   *     // Other code before the main script run starts...
+   *     Foo = require("./foo").Foo;
+   *
+   * This means that we can fill in the namespace entry early and then before anything really
+   * uses it we can insert the final value. Specifically, we can do that as soon as the
+   * exporting module body execution finishes.
+   *
+   * The initial "var" line has to appear at its original (presumably top) level; by doing so
+   * it inserts an entry directly into the correct namespace. It won't work inside a function.
+   * Once the name exists though, and plain assignment in any scope which inherits the
+   * original namespace is fine, so the second half is done via a callback function. It
+   * doesn't make sense to do a second assingment once the module's evaluation is complete, so
+   * at that point the callback handler is dummied out and any pending callbacks executed.
+   *
+   * On the exporting side, `export Foo` becomes:
+   *
+   *     module.exports.Foo = null;
+   *     // Other module code...
+   *     module.exports.Foo = Foo; // Original line position
+   *     // Other module code...
+   *
+   * This ensures that the names all exist before any other code's import returns, and sets
+   * the correct value once it's known. It's necessary to do this in two stages because only
+   * one type of value has both its declaration and value hoisted: explicitly named functions.
+   */
+
+  /**
+   * Returns injectable source to import the given destination-to-source
+   * map of names from the given file.
+   *
+   * @param {string} file
+   * @param {object} dest_to_src
+   * @return {string}
+   */
+  function importAll(file, dest_to_src) {
+    return `var ${Object.keys(dest_to_src).join(",")};` +
+    `require("${file}").importer(ns=>{` +
+    Object.keys(dest_to_src).map(dest =>
+      `${dest}=ns.${dest_to_src[dest]}`
+    ).join(";") +
+    `})`
   }
-  src = src.replace(/\bexport default +/g, 'module.exports.ns = ');
-  src = src.replace(/\bexport (var|let|const) ([a-zA-Z0-9_$]*)/g, '$1 $2 = module.exports.ns.$2');
+  src = src.replace(
+    /\bimport ([^{]*?) from (["'])(.*?)\2/g,
+    (all, $1, $2, $3) => importAll($3, {[$1]: "default"})
+  );
+  src = src.replace(
+    /\bimport {([^{]*?)} from (["'])(.*?)\2/g,
+    (all, $1, $2, $3) => {
+      var names = {};
+      $1.split(/,/).forEach(name => names[name.trim()] = null);
+      return importAll($3, names);
+    }
+  );
+
+  src = src.replace(/\bexport default +/g, 'module.exports.ns.default = ');
+  src = src.replace(/\bexport (var|let|const) ([a-zA-Z0-9_$]*)/g, (all, $1, $2) => {
+    return `module.exports.ns.${$2} = ${$1} ${$2}`;
+  });
   src = src.replace(
     /\bexport (function|class) ([a-zA-Z0-9_$]*)/g,
-    'var $2 = module.exports.ns.$2 = $1'
-);
+    'module.exports.ns.$2 = $1 $2'
+  );
+
   src = src.replace(/\bexport {(.*?)}/g, (all, $1) => {
-    var names = $1.split(/,/);
-    return names.map(
-        name => `module.exports.ns.${name.trim()} = ${names[name.trim()]}`
+    var names = identifierList($1);
+    return Object.keys(names).map(
+        k => `module.exports.ns.${k} = ${names[k]}`
     ).join(";");
-  return src;
+  });
+  if(src.match(/\bmodule.exports\b/)) {
+    return "var importers = []; " +
+      "module.exports.ns = {}; " +
+      "module.exports.importer = f => importers.push(f);" +
+      src + "\n" +
+      "module.exports.importer = f => f(module.exports.ns);" +
+      "importers.forEach(f => f(module.exports.ns));";
+  } else {
+    return src;
+  }
 });
