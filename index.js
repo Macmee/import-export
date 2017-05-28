afa3fa6736430c6c3924453f5576db4a35fee655
Jim Driscoll <jim.a.driscoll@gmail.com>
2017-05-28 15:49:03 +0100

    Put all the import work together

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ hook.hook('.js', (src, name) => {
     seen_names[$1.trim()] = $3;
     return `imported["${$3}"] = require("${$3}").ns`;
   });
+  src = src.replace(/\bimport {(.*?)} from (["'])(.*?)\2/g, (all, $1, $2, $3) => {
+    $1.split(",").forEach(name => {
+        seen_names[name.trim()] = $3;
+    });
+    return `imported["${$3}"] = require("${$3}").ns`;
+  });
+  if(Object.keys(seen_names).length) {
+      // Ultimately we want AST parsing but this will work in most cases.
+      Object.keys(seen_names).forEach(name => {
+        src = src.replace(new RegExp(`\\b${name}\\b`, "g"), `imported["${seen_names[name]}"].${name}`)
+      });
+      src = "var imported = {};" + src;
+  }
   src = src.replace(/\bexport default +/g, 'module.exports.ns = ');
   src = src.replace(/\bexport (var|let|const) ([a-zA-Z0-9_$]*)/g, '$1 $2 = module.exports.ns.$2');
   src = src.replace(
@@ -17,15 +30,5 @@ hook.hook('.js', (src, name) => {
     return names.map(
         name => `module.exports.ns.${name.trim()} = ${names[name.trim()]}`
     ).join(";");
-  src = src.replace(/\bimport {(.*?)} from (["'])(.*?)\2/g, (all, $1, $2, $3) => {
-    $1.split(",").forEach(name => {
-        seen_names[name.trim()] = $3;
-    });
-    return `imported["${$3}"] = require("${$3}").ns`;
-  });
-  // Ultimately we want AST parsing but this will work in most cases.
-  Object.keys(seen_names).forEach(name => {
-    src = src.replace(new RegExp(`\\b${name}\\b`, "g"), `imported["${seen_names[name]}"].${name}`)
-  });
-  return "var imported = {};" + src;
+  return src;
 });
