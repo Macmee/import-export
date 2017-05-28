bc86d0327407fbf47f8c739efb3172a64c4bd1fe
Jim Driscoll <jim.a.driscoll@gmail.com>
2017-05-28 15:22:10 +0100

    Put all the exports in a second-level namespace so that the top level is free for metadata or returnable helpers

--- index.js
+++ index.js
@@ -4,20 +4,22 @@ hook.hook('.js', (src, name) => {
   var seen_names = {};
   src = src.replace(/\bimport ([^{]*?) from (["'])(.*?)\2/g, (all, $1, $2, $3) => {
     seen_names[$1.trim()] = $3;
-    return `imported["${$3}"] = require("${$3}")`;
+    return `imported["${$3}"] = require("${$3}").ns`;
   });
-  src = src.replace(/\bexport default +/g, 'module.exports = ');
-  src = src.replace(/\bexport (var|let|const) ([a-zA-Z0-9_$]*)/g, '$1 $2 = module.exports.$2');
-  src = src.replace(/\bexport function ([a-zA-Z0-9_$]*)/g, 'var $1 = module.exports.$1 = function');
-  src = src.replace(/\bexport class ([a-zA-Z0-9_$]*)/g, 'var $1 = module.exports.$1 = class');
+  src = src.replace(/\bexport default +/g, 'module.exports.ns = ');
+  src = src.replace(/\bexport (var|let|const) ([a-zA-Z0-9_$]*)/g, '$1 $2 = module.exports.ns.$2');
+  src = src.replace(/\bexport function ([a-zA-Z0-9_$]*)/g, 'var $1 = module.exports.ns.$1 = function');
+  src = src.replace(/\bexport class ([a-zA-Z0-9_$]*)/g, 'var $1 = module.exports.ns.$1 = class');
   src = src.replace(/\bexport {(.*?)}/g, (all, $1) => {
-    return $1.split(/,/).map(name => `module.exports.${name.trim()} = ${name.trim()}`).join(";");
-  });
+    var names = $1.split(/,/);
+    return names.map(
+        name => `module.exports.ns.${name.trim()} = ${names[name.trim()]}`
+    ).join(";");
   src = src.replace(/\bimport {(.*?)} from (["'])(.*?)\2/g, (all, $1, $2, $3) => {
     $1.split(",").forEach(name => {
         seen_names[name.trim()] = $3;
     });
-    return `imported["${$3}"] = require("${$3}")`;
+    return `imported["${$3}"] = require("${$3}").ns`;
   });
   // Ultimately we want AST parsing but this will work in most cases.
   Object.keys(seen_names).forEach(name => {
