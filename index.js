a15316702028af72146eab8c0f035b14ca7aa8a3
Potseluyko Vyacheslav <vyacheslav.potseluyko@gmail.com>
2017-08-17 14:34:54 +0300

    Add async functions support

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ hook.hook('.js', (src, name) => {
   src = src.replace(/export default ([^ ]*)/g, 'module.exports = $1');
   src = src.replace(/export (var|let|const) ([a-zA-Z0-9_$]*)/g, '$1 $2 = module.exports.$2');
   src = src.replace(/export function ([a-zA-Z0-9_$]*)/g, 'var $1 = module.exports.$1 = function');
+  src = src.replace(/export async function ([a-zA-Z0-9_$]*)/g, 'var $1 = module.exports.$1 = async function');
   src = src.replace(/export class ([a-zA-Z0-9_$]*)/g, 'var $1 = module.exports.$1 = class');
   src = src.replace(/import {(.*?)} from ['"](.*?)['"]/g, (all, $1, $2) => {
     return $1.split(",")
