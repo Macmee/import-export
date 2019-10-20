419bad5291dd753ec20135e1317c13a85d44c230
Jim Driscoll <jim.a.driscoll@gmail.com>
2019-10-20 15:54:50 +0100

    Test the initial state of a deferrable require

--- test.js
+++ test.js
@@ -1,2 +1,7 @@
-require("./index");
-require("./test/import");
+const assert = require("assert")
+
+require("./index")
+require("./test/import")
+
+const a = require("./test/mutual-a")
+assert.ok(a.hasOwnProperty("A"), "mutual export is initially set")
