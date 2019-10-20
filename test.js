f9f78348b8511c1ec7992a30ffb84da2b0b2b00c
Jim Driscoll <jim.a.driscoll@gmail.com>
2019-10-20 15:57:12 +0100

    Test that the promise is resolved in reasonable time

--- test.js
+++ test.js
@@ -5,3 +5,16 @@ require("./test/import")
 
 const a = require("./test/mutual-a")
 assert.ok(a.hasOwnProperty("A"), "mutual export is initially set")
+const timeout = setTimeout(
+    () => console.error("Passed timeout without import resolving - this is a bug"),
+    5000
+)
+assert.doesNotReject(async () => {
+    try {
+        const ns = await a._bridge
+        assert.ok(ns.A, "mutual export is set to a real value after then()")
+    } catch(e) {
+        console.log(e)
+    }
+    clearTimeout(timeout)
+})
