f7c6bffa071c2a7a0eefedd79278dd704bcd2d95
Jim Driscoll <jim.a.driscoll@gmail.com>
2019-10-23 10:43:01 +0100

    Actually test the "var" counterpart

--- test.js
+++ test.js
@@ -2,3 +2,4 @@ require("./index")
 require("./test/import")
 require("./test/circular-definition")
 require("./test/export-class").Baz.boz // Just try evaluating it
+require("./test/export-class").Baz.bar // Just try evaluating it
--- test/export-class.js
+++ test/export-class.js
@@ -1,6 +1,10 @@
 export class Foo extends Error {}
+export var Foe = 1
 export class Baz {
     static get boz() {
         return new Foo("foo")
+    }
+    static get bar() {
+        return Foe.toFixed(2)
     }
 }
